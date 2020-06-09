ddb81ab989eb79b5526e45ced2cabc0925d0ad52
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-06-09 19:33:35 +0300

    new solution

--- Lesson 1 THIS/Lab/03. ArrayMap.js
+++ Lesson 1 THIS/Lab/03. ArrayMap.js
@@ -3,5 +3,12 @@ function arrayMap(arr, fn) {
     .reduce((a, b) => [...a, fn(b)], []);
 }
 
+function arrayMap2(arr, fn) {
+    return arr.reduce((a, b) => {
+        a.push(fn(b));
+        return a;
+    },[]);
+}
+
 let nums = [1,2,3,4,5];
 console.log(arrayMap(nums,(item)=> item * 2)); // [ 2, 4, 6, 8, 10 ]
