f8e05755cc83328b7ceaca8cfe1b6716dcf2164f
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-06-09 18:41:30 +0300

    new solution

--- Lesson 1 THIS/Lab/01. Area and Volume Calculator.js
+++ Lesson 1 THIS/Lab/01. Area and Volume Calculator.js
@@ -1,10 +1,10 @@
 function area() {
     return this.x * this.y;
-};
+}
 
 function vol() {
     return this.x * this.y * this.z;
-};
+}
 
 function solve(area, vol, input) {
     return JSON.parse(input)
@@ -12,8 +12,22 @@ function solve(area, vol, input) {
             return {
                 area: Math.abs(area.call(x)),
                 volume: Math.abs(vol.call(x))
-            }
-        })
+            };
+        });
+}
+
+function solve2(area, vol, input) {
+    const result = [];
+
+    const inputObjs = JSON.parse(input);
+    for (const obj of inputObjs) {        
+        result.push({
+            area: Math.abs(area.call(obj)),
+            volume: Math.abs(vol.call(obj))
+        });
+    }
+
+    return result;
 }
 
 console.log(solve(
