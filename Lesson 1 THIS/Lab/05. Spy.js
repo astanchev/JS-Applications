17b3f178357c2adc8b2faed351d26c8f73ac257c
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-06-11 10:14:53 +0300

    refactor

--- Lesson 1 THIS/Lab/05. Spy.js
+++ Lesson 1 THIS/Lab/05. Spy.js
@@ -8,18 +8,18 @@ function spy(obj, fn) {
     obj[fn] = function () {
         res.count++;
         return originalFN.apply(obj, arguments);
-    }
+    };
 
     return res;
 }
 
 let obj = {
     method:()=>"invoked"
-}
+};
 let spy1 = spy(obj,"method");
 
 obj.method();
 obj.method();
 obj.method();
 
-console.log(spy1) // { count: 3 }
+console.log(spy1); // { count: 3 }
