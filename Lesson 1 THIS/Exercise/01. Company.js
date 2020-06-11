e6bc15923866d2130415bfb00993e7652772e524
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-06-11 11:06:08 +0300

    refactor

--- Lesson 1 THIS/Exercise/01. Company.js
+++ Lesson 1 THIS/Exercise/01. Company.js
@@ -8,44 +8,46 @@ class Company {
             throw new Error('Invalid input!');
         }
 
-        let existingDep = this.departments.find(d => d.name === department)
+        let existingDep = this.departments.find(d => d.name === department);
 
         if (!existingDep) {
             existingDep = {
                 name: department,
                 employees: [],
                 totalSalaries: function () {
-                    return this.employees.reduce((a, b) => a+ b.salary,0);
+                    return this.employees.reduce((a, b) => a + b.salary, 0);
                 },
-                averageSalary: function() {
+                averageSalary: function () {
                     if (this.employees.length === 0) {
                         return 0;
                     } else {
                         return this.totalSalaries() / this.employees.length;
                     }
                 }
-            }
+            };
 
             this.departments.push(existingDep);
         }
 
-        existingDep.employees.push({username, salary, position});
+        existingDep.employees.push({
+            username,
+            salary,
+            position
+        });
 
         return `New employee is hired. Name: ${username}. Position: ${position}`;
     }
 
     bestDepartment() {
-        //with destructuring we take first
-        const [bestDepartment] = [...this.departments]
-                .sort((a, b) => b.averageSalary() - a.averageSalary());
-
+        const bestDepartment = this.departments
+                                .sort((a, b) => b.averageSalary() - a.averageSalary())[0];
         let result = `Best Department is: ${bestDepartment.name}\nAverage salary: ${bestDepartment.averageSalary().toFixed(2)}\n`;
-        
-        let emps = [...bestDepartment
-        .employees]
-        .sort((a, b) => b.salary - a.salary || (a.username).localeCompare(b.username))
-        .map(e => `${e.username} ${e.salary} ${e.position}`)
-        .join('\n');
+
+        let emps = bestDepartment
+            .employees
+            .sort((a, b) => b.salary - a.salary || (a.username).localeCompare(b.username))
+            .map(e => `${e.username} ${e.salary} ${e.position}`)
+            .join('\n');
 
         return result + emps;
     }
