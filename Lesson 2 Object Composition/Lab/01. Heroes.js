b79d2d8a478b6084cb449ebb074302daf9966b59
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-06-14 16:39:45 +0300

    refactor

--- Lesson 2 Object Composition/Lab/01. Heroes.js
+++ Lesson 2 Object Composition/Lab/01. Heroes.js
@@ -18,20 +18,20 @@ function solve() {
             name,
             health: 100,
             stamina: 100
-        }
+        };
 
         return Object.assign(state, canFight(state));
-    }
+    };
 
     const mage = (name) => {
         let state = {
             name,
             health: 100,
             mana: 100
-        }
+        };
 
         return Object.assign(state, canCast(state));
-    }
+    };
 
     return {
         mage: mage,
@@ -41,12 +41,12 @@ function solve() {
 
 let create = solve();
 const scorcher = create.mage("Scorcher");
-scorcher.cast("fireball")
-scorcher.cast("thunder")
-scorcher.cast("light")
+scorcher.cast("fireball");
+scorcher.cast("thunder");
+scorcher.cast("light");
 
 const scorcher2 = create.fighter("Scorcher 2");
-scorcher2.fight()
+scorcher2.fight();
 
 console.log(scorcher2.stamina);
 console.log(scorcher.mana);
