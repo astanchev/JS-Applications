81d3e53ebdb05a9c04d5a03488ed8191cdd9a04d
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-06-15 15:46:10 +0300

    refactor

--- Lesson 2 Object Composition/Exercise/06. Sorted List.js
+++ Lesson 2 Object Composition/Exercise/06. Sorted List.js
@@ -17,6 +17,21 @@ function sortedList() {
             if (index >= 0 && this.numbersList.length) {
                 return this.numbersList[index];
             }
-        }
-    }
+        },
+        print: function () {
+            console.log(this.numbersList.join(', '));
+        },
+    };
 }
+
+
+let a = sortedList();
+a.add(3);
+a.add(8);
+a.add(2);
+a.add(9);
+console.log(a.size);
+a.remove(1);
+console.log(a.size);
+a.print();
+console.log(a.get(2));
--- test.js
+++ test.js
@@ -1,7 +1,4 @@
 function solve() {
     
-}
-
-console.log(solve(
     
-));
+}
