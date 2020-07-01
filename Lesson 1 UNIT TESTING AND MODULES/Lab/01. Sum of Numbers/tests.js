3dee4c90c7e36ee5607addefd1742c8849c8b86f
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-07-01 21:55:29 +0300

    Add Lab problem 1

---
+++ Lesson 1 UNIT TESTING AND MODULES/Lab/01. Sum of Numbers/tests.js
@@ -0,0 +1,20 @@
+const sum = require("./index.js");
+const assert = require("chai").assert;
+
+describe("sum() tests", function() {
+  it("should return correct result if passed number array", function() {
+    let actual = sum([1, 2, 3]);
+    let expected = 6;
+    assert.equal(actual, expected);
+  });
+  it("should return correct result if passed number array with length === 1", function() {
+    let actual = sum([1]);
+    let expected = 1;
+    assert.equal(actual, expected);
+  });
+  it("should return correct result if passed number array with length === 0", function() {
+    let actual = sum([]);
+    let expected = 0;
+    assert.equal(actual, expected);
+  });
+});
