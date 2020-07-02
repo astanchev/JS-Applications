8af84e6c3a3da31e55f142cd558a1a8134298625
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-07-02 08:19:08 +0300

    Add lesson 1 exercise 1

---
+++ Lesson 1 UNIT TESTING AND MODULES/Exercise/01. Even or Odd/tests.js
@@ -0,0 +1,22 @@
+const isOddOrEven = require('./index.js');
+const assert = require('chai').assert;
+
+describe.only('isOddOrEven() tests', () => {
+    it('Should return undefined with a number parameter', () => {
+        assert.isUndefined(isOddOrEven(8));
+    });
+    it('Should return undefined with an object parameter', () => {
+        assert.isUndefined(isOddOrEven({name: 'George'}));
+    });
+    it('Should work correctly with string with even length', () => {
+        assert.equal(isOddOrEven('roar'), 'even');
+    });
+    it('Should work correctly with string with odd length', () => {
+        assert.equal(isOddOrEven('Peter'), 'odd');
+    });
+    it('Should return correct values with multiple consecutive checks', () => {
+        assert.equal(isOddOrEven('bird'), 'even');
+        assert.equal(isOddOrEven('cat'), 'odd');
+        assert.equal(isOddOrEven('dog'), 'odd');
+    });
+});
