07736ff3e7b1647f3f6fa8de8c0a5142dd43ac6e
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-07-03 21:38:08 +0300

    Add lesson 2 exercise 2

--- Lesson 2  REST SERVICES AND AJAX/Exercises/02.BUS-SCHEDULE/app.js
+++ Lesson 2  REST SERVICES AND AJAX/Exercises/02.BUS-SCHEDULE/app.js
@@ -1,11 +1,49 @@
 function solve() {
+    const info = document.querySelector('#info span.info');
+    const btnDepart = document.getElementById('depart');
+    const btnArrive = document.getElementById('arrive');
+
+    let nextStopName = 'Depot';
+    let nextStopId = 'depot';
 
     function depart() {
-        console.log('Depart TODO...');
+        switchDisable();
+
+        fetch(`http://localhost:3000/schedule/${nextStopId}.json`)
+            .then((res) => {
+                if (res.status !== 200) {
+                    throw res;
+                }
+                return res.json();
+            })
+            .then((data) => showNextInfo(data))
+            .catch((err) => handleError(err));
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        switchDisable();
+        info.textContent = `Arriving at ${nextStopName}`;
+    }
+
+    function showNextInfo(data) {
+        nextStopName = data.name;
+        nextStopId = data.next;
+        info.textContent = `Next stop ${nextStopName}`;
+    }
+
+    function handleError(err) {
+        info.textContent = 'Error';
+        disableButtons();
+    }
+
+    function switchDisable() {
+        btnDepart.disabled = btnDepart.disabled === true ? false : true;
+        btnArrive.disabled = btnArrive.disabled === true ? false : true;
+    }
+
+    function disableButtons() {
+        btnDepart.disabled = true;
+        btnArrive.disabled = true;
     }
 
     return {
