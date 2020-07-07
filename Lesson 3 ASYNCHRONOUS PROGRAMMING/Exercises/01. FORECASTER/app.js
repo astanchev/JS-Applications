dad14e789b3d0efdbf08f062d4f33812b246c4d7
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-07-08 00:01:25 +0300

    Add lesson 3 exercise 1

--- Lesson 3 ASYNCHRONOUS PROGRAMMING/Exercises/01. FORECASTER/app.js
+++ Lesson 3 ASYNCHRONOUS PROGRAMMING/Exercises/01. FORECASTER/app.js
@@ -1,5 +1,105 @@
-function attachEvents() {
-    console.log("TODO...");
-}
+import * as data from './data.js';
+import createElement from './dom.js';
 
-attachEvents();
+const symbols = {
+    'Sunny': '&#x2600;',
+    'Partly sunny': '&#x26C5;',
+    'Overcast': '&#x2601;',
+    'Rain': '&#x2614;',
+    'Degrees': '&deg;',
+};
+
+window.addEventListener('load', () => {
+    const mainDiv = document.querySelector('#forecast');
+    const todayDiv = document.querySelector('#current');
+    const upcomingDiv = document.querySelector('#upcoming');
+    const inputLocation = document.querySelector('#location');
+    const todayDivLabel = `<div class="label">Current conditions</div>`;
+    const upcomingDivLabel = `<div class="label">Three-day forecast</div>`;
+
+    document.querySelector('#submit').addEventListener('click', getForecast);
+
+    async function getForecast() {
+        todayDiv.innerHTML = todayDivLabel;
+        upcomingDiv.innerHTML = upcomingDivLabel;
+
+        const locationName = inputLocation.value;
+        let code = '';
+
+        try {
+            code = await data.getCode(locationName);            
+        } catch (error) {
+            mainDiv.innerHTML = `Error`;
+            mainDiv.style.display = 'block';            
+            return;
+        }
+
+        const todayRes = data.getToday(code);
+        const upcomingRes = data.getUpcoming(code);
+
+        const [today, upcoming] = [await todayRes, await upcomingRes];
+
+        const symbolSpan = createElement('span', '', {
+            className: 'condition symbol'
+        });
+        symbolSpan.innerHTML = symbols[today.forecast.condition];
+
+        const tempSpan = createElement('span', '', {
+            className: 'forecast-data'
+        });
+        tempSpan.innerHTML = `${today.forecast.low}${symbols.Degrees}/${today.forecast.high}${symbols.Degrees}`;
+
+        const todayEl = createElement('div', [
+            symbolSpan,
+            createElement('span', [
+                createElement('span', today.name, {
+                    className: 'forecast-data'
+                }),
+                tempSpan,
+                createElement('span', today.forecast.condition, {
+                    className: 'forecast-data'
+                })
+            ], {
+                className: 'condition'
+            })
+        ], {
+            className: 'forecasts'
+        });
+
+        todayDiv.appendChild(todayEl);
+
+        const forecastInfoDiv = createElement('div', upcoming.forecast.map(renderUpcoming), {
+            className: 'forecasts'
+        });
+
+        upcomingDiv.appendChild(forecastInfoDiv);
+
+        mainDiv.style.display = 'block';
+    }
+
+    function renderUpcoming(forecast) {
+        const symbolSpan = createElement('span', '', {
+            className: 'symbol'
+        });
+        symbolSpan.innerHTML = symbols[forecast.condition];
+
+        const tempSpan = createElement('span', '', {
+            className: 'forecast-data'
+        });
+        tempSpan.innerHTML = `${forecast.low}${symbols.Degrees}/${forecast.high}${symbols.Degrees}`;
+
+        const result = createElement('span', [
+            symbolSpan,
+            tempSpan,
+            createElement('span', forecast.condition, {
+                className: 'forecast-data'
+            })
+        ], {
+            className: 'upcoming'
+        });
+
+        return result;
+    }
+
+
+});
