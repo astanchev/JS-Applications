0f7754f3a6be1817a7d6d37853416a386aaae07d
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-07-17 21:01:31 +0300

    finished routedemo2

--- Lesson 6 ROUTING/Lab/RouterDemo2/app.js
+++ Lesson 6 ROUTING/Lab/RouterDemo2/app.js
@@ -1,24 +1,67 @@
-const appState = {
-    count: 0
-};
-const appRoot = 'http://localhost:8000/';
+const appState = { count: 0 };
+const appRoot = 'http://localhost:8000';
+const routesParams = {};
+const routes = {};
 
-function link1() {
-    document.all.content.innerHTML = 'Link 1 was pressed';
+function Page1(page, temp) {
+    document.title = "Page 1";
+    document.all
+        .content
+        .innerHTML = `Link 1 was pressed => page: ${page} | temp: ${temp}`;
 }
 
-document.addEventListener('click', function (e) {
+function Page2() {
+    document.title = "Page 2";
+    document.all
+        .content
+        .innerHTML = `Page 2!`;
+}
+
+function Page2_1() {
+    document.title = "Page 2_1";
+    document.all
+        .content
+        .innerHTML = `Page 2_1!`;
+}
+
+function createPath(path, callback) {
+    const processedPath = path.split(':');
+    const realPath = processedPath.splice(0, 1);
+
+    routes[realPath] = callback;
+    routesParams[realPath] = processedPath;
+}
+
+createPath('/link1:page:temp', Page1);
+createPath('/link2', Page2);
+createPath('/link2/subLink1', Page2_1);
+
+function clickHandler(e) {
     if (e.target.dataset.myLink === "1") {
         e.preventDefault();
-        history.pushState({
-                count: appState.count++
-            },
-            '',
-            e.target.href);
-        window[
-            e.target.href
-            .replace(appRoot, '')
-            .replace('/', '')
-        ]();
+        router(e.target.href);
     }
+}
+
+function router(href, isBack) {
+    if (!isBack) {
+        history.pushState({ count: appState.count++ }, '', href);
+    }
+
+    const q = [...new URLSearchParams(location.search).entries()]
+                .filter(x => routesParams[location.pathname].indexOf(x[0]) > -1)
+                .map(x => x[1]);
+
+    if (typeof routes[location.pathname] === 'function') {
+        routes[location.pathname](...q);
+    }
+}
+
+document.addEventListener('click', clickHandler);
+window.addEventListener('popstate', function (e) {
+    router(location.href, true);
 });
+window.addEventListener('load', function (e) {
+    router(location.href, false);
+});
+
