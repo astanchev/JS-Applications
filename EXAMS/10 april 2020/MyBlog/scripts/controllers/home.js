2bad96d655f93e0193698d7c1df12137743f7d34
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-07-25 12:42:40 +0300

    finish home, user controller, views + database

---
+++ EXAMS/10 april 2020/MyBlog/scripts/controllers/home.js
@@ -0,0 +1,11 @@
+import * as notifications from '../helpers/notifications.js';
+
+export default async function () {
+    this.partials = {
+        header: (await this.load('../../templates/common/header.hbs'))
+    };
+
+    this.partial('../../templates/home/home.hbs', this.app.userData);
+
+    notifications.hideLoader();
+}
