c2e24dee5c0e9beb5a485fbd71f710e928b76278
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-08-01 00:04:12 +0300

    refactor

--- Lesson 9 WORKSHOP CUSTOM COMPONENTS/Exercises/03. user-cards/index.js
+++ Lesson 9 WORKSHOP CUSTOM COMPONENTS/Exercises/03. user-cards/index.js
@@ -74,7 +74,7 @@ function getUserCardTemplate(context) {
       <div class="info">
         <h3>${context.name}</h3>
         ${userInfo}
-        <button @click=${context.toggleHandler} class="toggle-info-btn">Toggle Info</button>
+        <button @click=${context.toggleHandler} class="toggle-info-btn">${context.showInfo ? 'Hide Info' : 'Show Info'}</button>
       </div>
   </div>
   `;
