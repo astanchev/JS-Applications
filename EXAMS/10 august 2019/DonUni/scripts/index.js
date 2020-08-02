ee9cb4529627bf3a65b49c1725d2af2e5a41c676
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-08-02 13:44:48 +0300

    Finish home and user controllers

---
+++ EXAMS/10 august 2019/DonUni/scripts/index.js
@@ -0,0 +1,49 @@
+import home from './controllers/home.js';
+import * as users from './controllers/users.js';
+import * as notifications from './helpers/notifications.js';
+
+window.addEventListener('load', () => {
+    const app = Sammy('#root', function () {
+        this.use('Handlebars', 'hbs');
+
+        this.before('/', notifications.showLoader);
+        this.before('index.html', notifications.showLoader);
+        this.before('#/home', notifications.showLoader);
+
+        this.userData = {
+            email: localStorage.email || '',
+            userId: localStorage.userId || ''
+        };
+
+        this.get('index.html', home);
+        this.get('#/home', home);
+        this.get('/', home);
+
+        this.get('#/register', users.registerGet);
+        this.post('#/register', (ctx) => {
+            users.registerPost.call(ctx);
+        });
+
+        this.get('#/login', users.loginGet);
+        this.post('#/login', (ctx) => {
+            users.loginPost.call(ctx);
+        });
+
+        this.get('#/logout', users.logout);
+
+        // this.get('#/cause/create', cause.createGet);
+        // this.post('#/cause/create', (ctx) => {
+        //     cause.createPost.call(ctx);
+        // });
+
+        // this.get('#/cause/edit/:id', cause.editGet);
+        // this.post('#/cause/edit/:id', (ctx) => {
+        //     cause.editPost.call(ctx);
+        // });
+
+        // this.get('#/cause/details/:id', cause.details);
+        // this.get('#/cause/delete/:id', cause.deleteIdea);
+    });
+
+    app.run('/');
+});
---
+++ EXAMS/10 august 2019/DonUni/scripts/repository/data.js
@@ -0,0 +1,128 @@
+const url = `https://api.backendless.com/17084FAB-8F1D-5FD5-FF44-C5F0788E3100/E764E0B5-4CA3-44D1-939A-F82A4D01FE3F/`;
+
+const endpoints = {
+    register: 'users/register',
+    login: 'users/login',
+    logout: 'users/logout',
+    user: 'data/Users'
+};
+
+export async function register(user) {
+    return (await fetch(url + endpoints.register, {
+        method: 'post',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    })).json();
+}
+
+export async function login(user) {
+    return (await fetch(url + endpoints.login, {
+        method: 'post',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    })).json();
+}
+
+export async function logout(token) {
+    return await fetch(url + endpoints.logout, {
+        method: 'get',
+        headers: {
+            'user-token': token
+        }
+    });
+}
+
+// export async function createTrek(token, trek) {
+//     return await (await fetch(url + endpoints.trek, {
+//         method: 'post',
+//         headers: {
+//             'Content-type': 'application/json',
+//             'user-token': token
+//         },
+//         body: JSON.stringify(trek)
+//     })).json();
+// }
+
+// export async function editTrek(token, trekId, trek) {
+//     const trekURL = url + endpoints.trek + `/${trekId}`;
+
+//     return await (await fetch(trekURL, {
+//         method: 'put',
+//         headers: {
+//             'Content-type': 'application/json',
+//             'user-token': token
+//         },
+//         body: JSON.stringify(trek)
+//     })).json();
+// }
+
+// export async function deleteTrek(token, trekId) {
+//     const trekURL = url + endpoints.trek + `/${trekId}`;
+
+//     return (await fetch(trekURL, {
+//         method: 'delete',
+//         headers: {
+//             'user-token': token
+//         }
+//     })).json();
+// }
+
+// export async function likeTrek(token, trekId) {
+//     const trekURL = url + endpoints.trek + `/${trekId}`;
+
+//     const trek = await (await fetch(trekURL, {
+//         method: 'get',
+//         headers: {
+//             'user-token': token
+//         }
+//     })).json();
+
+//     if (trek.ownerId === localStorage.userId) {
+//         throw new Error('You can not like your trek!');
+//     }
+
+//     return await (await fetch(trekURL, {
+//         method: 'put',
+//         headers: {
+//             'Content-type': 'application/json',
+//             'user-token': token
+//         },
+//         body: JSON.stringify({
+//             likes: Number(trek.likes) + 1
+//         })
+//     })).json();
+// }
+
+// export async function getAllTreks(token) {
+
+//     return (await fetch(url + endpoints.trek, {
+//         headers: {
+//             'user-token': token
+//         }
+//     })).json();
+// }
+
+// export async function getMyTreks(token, userId) {
+//     const whereURL = url + endpoints.trek + `?where=ownerId%3D%27${userId}%27`;
+
+//     return (await fetch(whereURL, {
+//         headers: {
+//             'user-token': token
+//         }
+//     })).json();
+// }
+
+// export async function getTrekById(token, trekId) {
+//     const urlTrek = url + endpoints.trek + `/${trekId}`;
+
+//     return await (await fetch(urlTrek, {
+//         method: 'get',
+//         headers: {
+//             'user-token': token
+//         }
+//     })).json();
+// }
